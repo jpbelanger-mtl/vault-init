04f2550060bb4802e8bd3e9f82ace23031f59658
Jean-Philippe Bélanger <jean-philippe.belanger@appdirect.com>
2017-07-21 13:46:38 -0400

    Added rekey

--- vault-init.go
+++ vault-init.go
@@ -23,6 +23,7 @@ import (
 	vaultapi "github.com/hashicorp/vault/api"
 	vaultpgp "github.com/hashicorp/vault/helper/pgpkeys"
 	"golang.org/x/crypto/openpgp"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func sendEmails(smtpFrom string, smtpHost *string, gpgKey []byte, vaultKey string, clustername *string) {
@@ -32,8 +33,8 @@ func sendEmails(smtpFrom string, smtpHost *string, gpgKey []byte, vaultKey strin
 	}
 
 	//For each Identities present into that key, send an email to the recipient with his encrypted shared vault key
-	for k, v := range entitylist[0].Identities {
-		fmt.Printf("\t\tSending key %v to %s\n", k, v.UserId.Email)
+	for _, v := range entitylist[0].Identities {
+		fmt.Printf("\tSending to %s\n\t\t%v\n\n", v.UserId.Email, vaultKey)
 		sendEmail(smtpFrom, smtpHost, v.UserId.Email, vaultKey, clustername)
 	}
 }
@@ -75,9 +76,10 @@ func main() {
 	var smtpFrom = flag.String("smtpFrom", "", "From email address")
 	var smtpHost = flag.String("smtpHost", "localhost:25", "SMTP host to use in <host>:<port> format")
 	var vaultURL = flag.String("vaultURL", "http://127.0.0.1:8200", "Vault cluster url in http(s)://<host>:<port> format")
-	var secretShares = flag.Int("secretShares", 5, "Number of secret shared key to generate")
 	var secretThreshold = flag.Int("secretThreshold", 3, "Secret threshold for unsealing the vault")
 	var keybaseList = flag.String("keybase", "", "Comma-separated list of keybase account to fetch key from")
+	var rekey = flag.Bool("rekey", false, "This will be a rekey operation")
+	var nonce = flag.String("noonce", "", "Nonce used for rekey progress")
 
 	flag.Parse()
 
@@ -96,6 +98,8 @@ func main() {
 		print("keybase must contains at least one account name")
 		os.Exit(1)
 	}
+
+	var secretShares = len(keybases)
 
 	fmt.Printf("Initializing vault client\n")
 	vaultconfig := vaultapi.DefaultConfig()
@@ -116,22 +120,16 @@ func main() {
 
 	fmt.Printf("Vault is initialized: %v\n", initialized)
 
+	if *rekey && !initialized {
+		fmt.Printf("Cluster is NOT initialized, can't do a rekey operation\n")
+		os.Exit(1)
+	}
+
 	if !initialized {
 		initRequest := vaultapi.InitRequest{
-			SecretShares:    *secretShares,
+			SecretShares:    secretShares,
 			SecretThreshold: *secretThreshold,
-		}
-
-		splitValues := strings.Split(*keybaseList, ",")
-		keybaseMap, err := vaultpgp.FetchKeybasePubkeys(splitValues)
-
-		for _, keyfile := range splitValues {
-			key := keybaseMap[keyfile]
-			if key == "" {
-				fmt.Printf("key for keybase user %s was not found in the map", keyfile)
-			}
-
-			initRequest.PGPKeys = append(initRequest.PGPKeys, key)
+			PGPKeys:         getPGPKeys(keybases),
 		}
 
 		fmt.Printf("\nVault will be initialized with the following config: \n")
@@ -157,7 +155,80 @@ func main() {
 			sendEmails(*smtpFrom, smtpHost, decodedKey, keyB64, vaultURL)
 		}
 		fmt.Printf("\nProcessing done...\n")
+	} else if *rekey {
+		fmt.Printf("Cluster is already initialized, this will be a rekey operation\n")
+		rekeyStatus, err := vault.Sys().RekeyStatus()
+		if err != nil {
+			log.Fatal(err)
+		}
+		rekeyInitRequest := vaultapi.RekeyInitRequest{
+			SecretShares:    secretShares,
+			SecretThreshold: *secretThreshold,
+			PGPKeys:         getPGPKeys(keybases),
+		}
+		if !rekeyStatus.Started {
+			fmt.Printf("Rekey not started, initializing\n")
+
+			fmt.Printf("\nVault rekey with the following config: \n")
+			fmt.Printf("Total secret shares: %v\n", rekeyInitRequest.SecretShares)
+			fmt.Printf("Secret threshold: %v\n", rekeyInitRequest.SecretThreshold)
+			fmt.Printf("Number of GPG keys: %v\n\n", len(rekeyInitRequest.PGPKeys))
+			rekeyResponse, err := vault.Sys().RekeyInit(&rekeyInitRequest)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Rekey started: %v\n", rekeyResponse.Started)
+			fmt.Printf("Rekey nonce: %v\n", rekeyResponse.Nonce)
+			nonce = &rekeyResponse.Nonce
+		}
+
+		rekeyStatus, err = vault.Sys().RekeyStatus()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if rekeyStatus.Started {
+			fmt.Print("Rekey in progress, please enter your unseal key: ")
+			unsealKey, err := terminal.ReadPassword(0)
+			fmt.Print("\n")
+			if err != nil {
+				log.Fatal(err)
+			}
+			rekeyUpdateResponse, err := vault.Sys().RekeyUpdate(string(unsealKey), *nonce)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if rekeyUpdateResponse.Complete {
+				for index, keyB64 := range rekeyUpdateResponse.Keys {
+					//Decoding the base64 (non-armored) PGP key into binary
+					decodedKey, err := base64.StdEncoding.DecodeString(rekeyInitRequest.PGPKeys[index])
+					if err != nil {
+						panic(err)
+					}
+					sendEmails(*smtpFrom, smtpHost, decodedKey, keyB64, vaultURL)
+				}
+			}
+		}
 	} else {
 		fmt.Printf("Cluster is already initialized, nothing to do\n")
 	}
+}
+
+func getPGPKeys(keybases []string) []string {
+	var PGPKeys []string
+	keybaseMap, err := vaultpgp.FetchKeybasePubkeys(keybases)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, keyfile := range keybases {
+		key := keybaseMap[keyfile]
+		if key == "" {
+			fmt.Printf("key for keybase user %s was not found in the map", keyfile)
+			os.Exit(1)
+		}
+
+		PGPKeys = append(PGPKeys, key)
+	}
+
+	return PGPKeys
 }
